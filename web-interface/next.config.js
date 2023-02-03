1aa328270c4eb6d1a0181818073c54ed162dbdb6
Suhel Kapadia <suhelkapadia2@gmail.com>
2023-02-03 12:09:03 +0530

    spheron config add

--- web-interface/next.config.js
+++ web-interface/next.config.js
@@ -1,26 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
+  swcMinify: true,
 }
 
 // next.config.js
 module.exports = {
-    ...nextConfig,
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-          plugins: ['@babel/plugin-syntax-dynamic-import']
-        }
-      }
-    });
-
-    return config;
-  }
-};
+  ...nextConfig,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  trailingSlash: true,
+}
 
